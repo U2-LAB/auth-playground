570e901d86fd2980a1e2abe420c251802487c08a
Nikita Efremov <nikefremov.00.008@hotmail.com>
2020-11-23 18:36:41 +0300

    v 0.6
    Add MutiFieldChoices of permissions in admin panel
    Create request for change this permission

--- source-of-truth/source_of_truth/auth_system_api/urls.py
+++ source-of-truth/source_of_truth/auth_system_api/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
-from .views import Auth
+from .views import Auth, Permission
 
 urlpatterns = [
-    path('Auth/',Auth)
+    path('Auth/',Auth),
+    path('Permission/', Permission)
 ]
--- source-of-truth/source_of_truth/auth_system_api/views.py
+++ source-of-truth/source_of_truth/auth_system_api/views.py
@@ -3,10 +3,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
 
-
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.backends import ModelBackend
+from django.utils.datastructures import MultiValueDictKeyError
 
+from urllib.parse import urlparse, parse_qs
+
+from .models import User
 
 @api_view(['POST'])
 def Auth(request):
@@ -20,7 +23,14 @@ def Auth(request):
                 "Permission": 'READ',
             }
         )
-    user = authenticate(username=request.data['username'], password=request.data['password'])
+    try:
+        user = authenticate(username=request.data['username'], password=request.data['password'])
+    except MultiValueDictKeyError as e:
+        return Response(
+            {
+                "ErrorCode": 'Error in Key name. Must be `username` and `password`',
+            }
+        )
     if user is not None:
         login(request, user)
 
@@ -40,3 +50,44 @@ def Auth(request):
             }
         )
 
+@api_view(['POST'])
+def Permission(request):
+    url = parse_qs(urlparse(request.build_absolute_uri()).query) # parse QueryString from url
+    try:
+        user_id = int(url['user_id'][0])
+    except KeyError:
+        return Response(
+            {
+                "ErrorCode": "KeyError querystring must be contain `user_id`",
+            }
+        )
+    try:
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return Response(
+            {
+                "ErrorCode": "User does not exist",
+            }
+        )
+    if user:
+        try:
+            allowed_fields = request.data['fields'].split(' ')
+            user.allow_fields = allowed_fields
+            user.save()
+            return Response(
+                {
+                    "Success": 'Fields was changed',
+                }
+            )
+        except (MultiValueDictKeyError, KeyError) as e:
+           return Response(
+            {
+                "ErrorCode": 'Error in Key name. Must be `permission` and `fields`',
+            }
+        ) 
+    else:
+        return Response(
+            {
+                "ErrorCode": "You are not logged in",
+            }
+        )
