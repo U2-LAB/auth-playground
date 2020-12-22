c502de0d75c61e5fec8956737e7698dc9e6f900b
Nikita Efremov <nikefremov.00.008@hotmail.com>
2020-12-22 11:28:25 +0300

    Add Faker to the fill_db management commands
    Now no need to specify the path to the pre-generated surnames and first names

--- source-of-truth/source_of_truth/auth_system_api/management/commands/fill_users.py
+++ source-of-truth/source_of_truth/auth_system_api/management/commands/fill_users.py
@@ -1,14 +1,18 @@
 import random
+from faker.providers.person.en import Provider
+from random import shuffle, seed, choice
 
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
+
 from progress.bar import Bar
 
 
 class Command(BaseCommand):
     help = 'Fill django_db database with User info'
     USER_COUNT = 1000
-
+    
     def _get_phone(self):
         count_number = 7
         phone = '+37529'
@@ -16,40 +20,40 @@ class Command(BaseCommand):
             phone = "".join([phone, str(random.randint(0, 9))])
         return phone
 
-    def add_arguments(self, parser):
-        parser.add_argument('first_name', nargs='?', type=str)
-        parser.add_argument('last_name', nargs='?', type=str)
-
     def handle(self, *args, **options):
         bar = Bar('Create user', max=self.USER_COUNT)
-        with open(options['first_name'], 'r') as first_name_file, open(options['last_name'], 'r') as last_name_file:
+        User = get_user_model()
+        first_names = list(set(Provider.first_names))[0:self.USER_COUNT]
+        seed(1234124)
+        shuffle(first_names)
 
-            first_name = None
-            last_name = None
-            email = None
-            phone = None
-            skype = None
-            password = None
+        first_name = None
+        last_name = None
+        email = None
+        phone = None
+        skype = None
+        password = None
 
-            for _ in range(self.USER_COUNT):
-                first_name = first_name_file.readline().strip()
-                last_name = last_name_file.readline().strip()
-                email = first_name + "@mail.ru"
-                phone = self._get_phone()
-                skype = ".".join([first_name, last_name])
-                password = first_name
-                try:
-                    new_user = get_user_model().objects.create_user(
-                        username=first_name,
-                        password=password,
-                        email=email,
-                        phone=phone,
-                        skype=skype,
-                        first_name=first_name,
-                        last_name=last_name,
-                    )
-                    new_user.save()
-                except ValueError:
-                    print('Duplicate username')
-                bar.next()
+        for first_name in first_names:
+            first_name = first_name
+            last_name = choice(first_names)
+            email = first_name + "@mail.ru"
+            phone = self._get_phone()
+            skype = ".".join([first_name, last_name])
+            password = first_name
+            try:
+                new_user = User.objects.create_user(
+                    username=first_name,
+                    password=password,
+                    email=email,
+                    phone=phone,
+                    skype=skype,
+                    first_name=first_name,
+                    last_name=last_name,
+                )
+                new_user.save()
+                
+            except (ValueError, IntegrityError):
+                print("duplicate")
+            bar.next()
         bar.finish()
